03f1fa879b90196890a3c0e56347d6742db3f145
Giuppe <giuseppe.da@gmail.com>
2021-10-09 20:46:23 +0200

    better exit animation

--- source/PlayerStateExited.hx
+++ source/PlayerStateExited.hx
@@ -3,6 +3,8 @@ package;
 import flixel.math.FlxPoint;
 import flixel.tweens.FlxTween;
 
+using modifications.FlxSpriteMod;
+
 class PlayerStateExited implements PlayerState
 {
 	public function new() {}
@@ -20,18 +22,21 @@ class PlayerStateExited implements PlayerState
 	public function onEnter(player:Player)
 	{
 		var exit = Registry.currentPlayState.exit;
-		var point1 = FlxPoint.get(exit.x + exit.width - player.width * 2, exit.y + exit.height - player.height);
-		if (exit.x + exit.width / 2 < player.x + player.width / 2)
+		var point1 = FlxPoint.get(exit.right() - player.width * 2, exit.bottom() - player.height);
+		if (exit.xCenter() < player.xCenter())
 		{
-			point1.x = exit.x + exit.width;
+			point1.x = exit.right();
 			player.animation.play("left");
-			FlxTween.linearPath(player, [point1, FlxPoint.get(point1.x - 16, point1.y)]);
+			FlxTween.linearMotion(player, player.x, player.y, point1.x, point1.y, 0.1)
+				.then(FlxTween.linearPath(player, [point1, FlxPoint.get(point1.x - 16, point1.y)]));
 		}
 		else
 		{
-			FlxTween.linearPath(player, [point1, FlxPoint.get(point1.x + 16, point1.y)]);
+			player.animation.play("right");
+			FlxTween.linearMotion(player, player.x, player.y, point1.x, point1.y, 0.1)
+				.then(FlxTween.linearPath(player, [point1, FlxPoint.get(point1.x + 16, point1.y)]));
 		}
-		FlxTween.tween(player, {alpha: 0}, 1.0);
+		FlxTween.num(0, 1, 0.1).then(FlxTween.tween(player, {alpha: 0}, 1.0));
 	}
 
 	public function onExit(player:Player) {}
