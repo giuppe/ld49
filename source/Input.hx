6ac9e92fdb0df574301f850313826ca9635c045a
Giuppe <giuseppe.da@gmail.com>
2021-10-10 13:29:15 +0200

    touch input in html5

--- source/Input.hx
+++ source/Input.hx
@@ -2,6 +2,18 @@ package;
 
 import flixel.FlxG;
 import flixel.input.keyboard.FlxKey;
+import haxe.Json;
+#if js
+import js.Syntax;
+#end
+
+@:structInit
+class TouchInfo
+{
+	public var identifier:Dynamic;
+	public var pageX:Float;
+	public var pageY:Float;
+}
 
 class Input
 {
@@ -14,6 +26,8 @@ class Input
 	public static var isLeft:Bool = false;
 	public static var isRight:Bool = false;
 	public static var isJump:Bool = false;
+
+	public static var ongoingTouches:Array<TouchInfo> = new Array<TouchInfo>();
 
 	public static function update(elapsed:Float)
 	{
@@ -61,6 +75,38 @@ class Input
 		{
 			isJustStart = true;
 		}
+
+		#if js
+		var touched = false;
+		var leftTouched = false;
+		var rightTouched = false;
+		var jumpTouched = false;
+		for (t in ongoingTouches)
+		{
+			touched = true;
+			if (t.pageY > 128)
+			{
+				if (t.pageX < 240)
+					leftTouched = true;
+				else
+					rightTouched = true;
+			}
+			else
+			{
+				jumpTouched = true;
+			}
+		}
+
+		if (touched)
+			isJustStart = true;
+		if (jumpTouched)
+			isJump = isJustJump = true;
+		if (leftTouched)
+			isJustLeft = isLeft = true;
+		if (rightTouched)
+			isJustRight = isRight = true;
+		#end
+
 		#if (!desktop || html5)
 		for (touch in FlxG.touches.list)
 		{
@@ -71,4 +117,102 @@ class Input
 		}
 		#end
 	}
+
+	#if js
+	public static function handleTouchStart(evt:Dynamic)
+	{
+		evt.preventDefault();
+		trace("touchstart.");
+		var touches = evt.changedTouches;
+
+		for (i in 0...touches.length)
+		{
+			trace("touchstart:" + i + "...");
+			ongoingTouches.push(copyTouch(touches[i]));
+		}
+	}
+
+	public static function handleTouchMove(evt:Dynamic)
+	{
+		evt.preventDefault();
+
+		var touches = evt.changedTouches;
+
+		for (i in 0...touches.length)
+		{
+			var idx = ongoingTouchIndexById(touches[i].identifier);
+
+			if (idx >= 0)
+			{
+				trace("continuing touch " + idx);
+
+				ongoingTouches.splice(idx, 1);
+				ongoingTouches[idx] = copyTouch(touches[i]);
+				trace(".");
+			}
+			else
+			{
+				trace("can't figure out which touch to continue");
+			}
+		}
+	}
+
+	public static function handleTouchEnd(evt:Dynamic)
+	{
+		evt.preventDefault();
+		trace("touchend");
+
+		var touches = evt.changedTouches;
+
+		for (i in 0...touches.length)
+		{
+			var idx = ongoingTouchIndexById(touches[i].identifier);
+
+			if (idx >= 0)
+			{
+				ongoingTouches.splice(idx, 1); // remove it; we're done
+			}
+			else
+			{
+				trace("can't figure out which touch to end");
+			}
+		}
+	}
+
+	public static function handleTouchCancel(evt)
+	{
+		evt.preventDefault();
+		trace("touchcancel.");
+		var touches = evt.changedTouches;
+
+		for (i in 0...touches.length)
+		{
+			var idx = ongoingTouchIndexById(touches[i].identifier);
+			ongoingTouches.splice(idx, 1); // remove it; we're done
+		}
+	}
+
+	public static function copyTouch(touch:Dynamic):TouchInfo
+	{
+		return {
+			identifier: touch.identifier,
+			pageX: touch.pageX,
+			pageY: touch.pageY
+		};
+	}
+
+	public static function ongoingTouchIndexById(idToFind):Int
+	{
+		for (i in 0...ongoingTouches.length)
+		{
+			var id = ongoingTouches[i].identifier;
+
+			if (id == idToFind)
+			{
+				return i;
+			}
+		}
+		return -1; // not found
+	}
+	#end
 }
--- source/Main.hx
+++ source/Main.hx
@@ -9,5 +9,11 @@ class Main extends Sprite
 	{
 		super();
 		addChild(new FlxGame(480, 256, InitState, true));
+		#if js
+		js.Syntax.code("document.body.addEventListener(\"touchstart\", {0} );", js.Syntax.field(Input, "handleTouchStart"));
+		js.Syntax.code("document.body.addEventListener(\"touchmove\", {0} );", js.Syntax.field(Input, "handleTouchMove"));
+		js.Syntax.code("document.body.addEventListener(\"touchend\", {0} );", js.Syntax.field(Input, "handleTouchEnd"));
+		js.Syntax.code("document.body.addEventListener(\"touchcancel\", {0} );", js.Syntax.field(Input, "handleTouchCancel"));
+		#end
 	}
 }
